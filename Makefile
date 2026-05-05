cd544ee5107aa8ab1bcab257133506fb3a599bf3
snopy229 <jastcraft278@gmail.com>
2026-05-05 18:56:10 +0300

    first commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+local-run:
+	python manage.py runserver
---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-t9lzj0nevx&+*39%^f8^e3lf4admbc-r71&j518*4b7^7p3%(z"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
