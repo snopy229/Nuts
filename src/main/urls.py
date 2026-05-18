79799988f83b783f4203e68917e289c88d91e432
snopy229 <jastcraft278@gmail.com>
2026-05-18 09:17:59 +0300

    fix: main urls

--- src/main/urls.py
+++ src/main/urls.py
@@ -5,5 +5,5 @@ from src.main import views
 app_name = "main"
 
 urlpatterns = [
-    path("", views.MainPageTemplateView.as_view()),
+    path("", views.MainPageTemplateView.as_view(), name="home"),
 ]
