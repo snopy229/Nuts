1803adea134ebd0f322c31541951767fb5e5cfff
snopy229 <jastcraft278@gmail.com>
2026-05-19 13:39:06 +0300

    add: video block

--- src/core/blocks.py
+++ src/core/blocks.py
@@ -1,6 +1,13 @@
 from wagtail import blocks
+from wagtailmedia.blocks import VideoChooserBlock
 
 
 class CardBlock(blocks.StructBlock):
     title = blocks.CharBlock()
     description = blocks.CharBlock()
+
+
+class VideoBlock(blocks.StructBlock):
+    title = blocks.CharBlock()
+    description = blocks.CharBlock()
+    video = VideoChooserBlock()
