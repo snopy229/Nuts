efb4681b42a75b2f35a4cc9b29a0dd9b73af5796
snopy229 <jastcraft278@gmail.com>
2026-05-19 18:37:04 +0300

    add: garden info model

---
+++ src/core/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.shortcuts import redirect
+from django.urls import reverse
+from wagtail.fields import StreamField
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.views.snippets import SnippetViewSet
+
+from src.core.blocks import GardenInfoBlock
+
+
+class GardenInfo(models.Model):
+    tab = StreamField(
+        [("block", GardenInfoBlock())],
+        use_json_field=True,
+        max_num=4,
+    )
+
+    def __str__(self):
+        return "Информация о саде"
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def get(cls):
+        obj, _ = cls.objects.get_or_create(pk=1)
+        return obj
+
+    class Meta:
+        verbose_name = "Информация о саде"
+
+
+class GardenInfoViewSet(SnippetViewSet):
+    model = GardenInfo
+    menu_label = "Информация о саде"
+    icon = "image"
+    add_to_admin_menu = True
+
+    def index_view(self, request):
+        gallery = GardenInfo.get()
+        url = reverse(f"{self.url_namespace}:edit", args=[gallery.pk])
+        return redirect(url)
+
+
+register_snippet(GardenInfo, viewset=GardenInfoViewSet)
