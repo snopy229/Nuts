97c84e8a5ae8733b53e83243d2efd1cfa0efc025
snopy229 <jastcraft278@gmail.com>
2026-05-23 14:27:59 +0300

    add: NewsAndArticlesPage

--- src/news_and_articles/models.py
+++ src/news_and_articles/models.py
@@ -1,1 +1,17 @@
 # Create your models here.
+from django.db import models
+from wagtail.admin.panels import FieldPanel
+from wagtail.models import Page
+
+
+class NewsAndArticlesPage(Page):
+    page_title = models.CharField()
+    description = models.TextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel("page_title", heading="Верхний баннер"),
+        FieldPanel("description", heading="Оплата"),
+    ]
+    max_count = 1
+    parent_page_types = ["wagtailcore.Page"]
+    template = "news_and_articles.html"
