18195e8fedd0011f110346050843ec923581de8a
snopy229 <jastcraft278@gmail.com>
2026-05-29 15:00:08 +0300

    add: GalleryTR

---
+++ src/gallery/translation.py
@@ -0,0 +1,14 @@
+from modeltranslation.decorators import register
+from modeltranslation.translator import TranslationOptions
+
+from .models import GalleryPage, Gallery
+
+
+@register(GalleryPage)
+class GalleryPageTR(TranslationOptions):
+    fields = "banner"
+
+
+@register(Gallery)
+class GalleryTR(TranslationOptions):
+    fields = "content"
