d75742bd1c401ed2daf5af3ac854f80f5606cc03
snopy229 <jastcraft278@gmail.com>
2026-06-02 23:38:58 +0300

    Here is a concise commit message summarizing the changes in the provided diff:
    ```text
    add: Transaction model
    
    - Create a new `Transaction` model with a foreign key relationship to the `Orders` model.
    - Add `cost` and `created_at` fields to store transaction details.
    - Import `models` from `django.db` and the `Orders` model from `src.checkouts.models`.
    ```

--- src/transaction/models.py
+++ src/transaction/models.py
@@ -1,1 +1,10 @@
+from django.db import models
+
+from src.checkouts.models import Orders
+
+
 # Create your models here.
+class Transaction(models.Model):
+    order = models.ForeignKey(Orders, on_delete=models.CASCADE, related_name="transactions")
+    cost = models.IntegerField(verbose_name="Стоимость")
+    created_at = models.DateTimeField(auto_now_add=True)
