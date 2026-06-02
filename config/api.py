f7a62534c5aa59e2b5516e3927b9331336d663ee
snopy229 <jastcraft278@gmail.com>
2026-06-02 12:03:09 +0300

    Here is a concise commit message summarizing the changes in the provided diff:
    ```text
    fix: checkouts router configuration in api
    
    - Import `checkouts_router` from `src.checkouts.router`.
    - Correct the `/checkouts` route to use `checkouts_router` instead of the mistakenly assigned `products_router`.
    ```

--- config/api.py
+++ config/api.py
@@ -2,8 +2,9 @@ from ninja import NinjaAPI
 
 from src.user.router import router as user_router
 from src.products.router import router as products_router
+from src.checkouts.router import router as checkouts_router
 
 api = NinjaAPI()
 api.add_router("/user", user_router, tags=["user"])
 api.add_router("/products", products_router, tags=["products"])
-api.add_router("/checkouts", products_router, tags=["checkouts"])
+api.add_router("/checkouts", checkouts_router, tags=["checkouts"])
