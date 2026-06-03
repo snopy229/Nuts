cdff7c6600eac1e0062d8d56220eaea9cc23c4ce
snopy229 <jastcraft278@gmail.com>
2026-06-03 14:44:57 +0300

    Here is a concise commit message summarizing the changes in the provided diff:
    ```text
    update: NEW_POST label in DeliveryType enum
    
    - Update the Russian translation label for the `NEW_POST` option in the `DeliveryType` enum to include clarification about payment by the client in Ukraine.
    ```

--- src/checkouts/enum/delivert_type.py
+++ src/checkouts/enum/delivert_type.py
@@ -3,6 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class DeliveryType(models.TextChoices):
-    NEW_POST = "new_post", _("Новая почта")
+    NEW_POST = "new_post", _("Новая почта (по Украине оплата за счет Клиента)")
     COURIER = "courier", _("Курьер по Одессе")
     PICKUP = "pickup", _("Самовывоз")
