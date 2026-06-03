f72fc831b7a5a1c015aaa43cef92cf93aa84ff53
snopy229 <jastcraft278@gmail.com>
2026-06-03 16:41:51 +0300

    Here is a concise commit message summarizing the changes in the provided diff:
    ```text
    update: add cost field and product_count property
    
    - Import `Sum` from `django.db.models`.
    - Add a `cost` field as a `PositiveIntegerField` with a translated verbose name to store the order cost.
    - Add a `product_count` property to calculate the total quantity of items in the `cart` using aggregation.
    ```

--- src/transaction/models.py
+++ src/transaction/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 from src.checkouts.models import Orders
 from src.transaction.enum.transaction_status import TransactionStatus
+from src.user.models import User
 
 
 # Create your models here.
 class Transaction(models.Model):
     order = models.ForeignKey(Orders, on_delete=models.CASCADE, related_name="transactions")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="transactions")
     cost = models.IntegerField(verbose_name="Стоимость")
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(
