82d8d5fdee474a5a5b33e0875eec84a693c374b1
snopy229 <jastcraft278@gmail.com>
2026-06-06 18:28:53 +0300

    add: order detail URL pattern
    - Import `OrderDetailView` from `src.checkouts.views`.
    - Add a new URL path for `"order/<int:pk>/"` to the URL routing configuration.
    - Map the new route to the `OrderDetailView` view and set the name to `"order_detail"`.

--- src/checkouts/urls.py
+++ src/checkouts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from src.checkouts.views import CartListView, OrderCreateView
+from src.checkouts.views import CartListView, OrderCreateView, OrderDetailView
 
 app_name = "checkouts"
 
 urlpatterns = [
     path("cart/", CartListView.as_view(), name="cart"),
     path("order/", OrderCreateView.as_view(), name="order"),
+    path("order/<int:pk>/", OrderDetailView.as_view(), name="order_detail"),
 ]
